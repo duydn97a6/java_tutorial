a3cfcf408699127e360d647a5a310092a7c25677
DuyDN <duydn@gemvietnam.com>
2023-11-10 10:28:12 +0700

    oke

---
+++ microservices/account/src/main/java/com/account/util/Constant.java
@@ -0,0 +1,9 @@
+package com.account.util;
+
+public interface Constant {
+
+    interface Regex {
+        String SEARCH_OPERATOR = "(\\w+?)(:|<|>)(.*)";
+        String SORT_OPERATOR = "(\\w+?)(:)(.*)";
+    }
+}
